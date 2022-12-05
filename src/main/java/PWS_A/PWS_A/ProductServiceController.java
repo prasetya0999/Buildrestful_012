9e3c073c4dd882d86b780be5032044d83f32fda4
Administrator <Administrator@WIN-171A8TGB90A>
2022-12-05 17:19:14 +0700

    commit 3 Signed-off-by:Administrator <Administrator@WIN-171A8TGB90A>

---
+++ src/main/java/PWS_A/PWS_A/ProductServiceController.java
@@ -0,0 +1,66 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package PWS_A.PWS_A;
+
+import java.util.HashMap;
+import java.util.Map;
+import model.Product;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author Administrator
+ */
+@RestController //membuat RestController
+public class ProductServiceController {
+    private static Map<String, Product> productRepo = new HashMap<>();//Membuat HasMap untuk menyimpan data pada Product.Java
+    static {
+        
+        Product honey = new Product(); //Membuat variabel honey
+        honey.setId("1"); //Memasukkan Id
+        honey.setName("Honey"); //Memasukkan nama 
+        productRepo.put(honey.getId(), honey);
+        
+        Product almond = new Product();//Membuat variabel almond
+        almond.setId("2");//Memasukkan id
+        almond.setName("Almond");//Memasukkan nama
+        productRepo.put(almond.getId(),almond);
+    }
+    
+    //Menampilkan page "/products"
+    @RequestMapping(value = "/products")
+    public ResponseEntity<Object> getProduct(){
+        return new ResponseEntity<>(productRepo.values(),HttpStatus.OK);
+    }
+    
+    //Untuk Menambahkan data pada page "/products"
+    @RequestMapping(value = "/products", method = RequestMethod.POST)
+    public ResponseEntity<Object> createProduct(@RequestBody Product product){
+        productRepo.put(product.getId(), product);
+        return new ResponseEntity<>("Product is created successfully", HttpStatus.CREATED);
+    }
+    
+    //Untuk Mengedit data pada page "/products"
+    @RequestMapping(value = "/products/{id}", method = RequestMethod.PUT)
+    public ResponseEntity<Object> updateProduct(@PathVariable("id") String id, @RequestBody Product product){
+        productRepo.remove(id);
+        product.setId(id);
+        productRepo.put(id, product);
+        return new ResponseEntity<>("Product is update seccessfully", HttpStatus.OK);
+    }
+    
+    //Untuk Menghapus data pada page "/products"
+    @RequestMapping(value = "/products/{id}", method = RequestMethod.DELETE)
+    public ResponseEntity<Object> delete(@PathVariable("id") String id){
+        productRepo.remove(id);
+        return new ResponseEntity<>("Product is deleted successfully", HttpStatus.OK);
+    }
+}
