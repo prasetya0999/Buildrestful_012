68ce33774906f1bb671d50e89df72ab03df01a6a
Administrator <Administrator@WIN-171A8TGB90A>
2022-12-05 17:16:57 +0700

    commit 1 Signed-off-by:Administrator <Administrator@WIN-171A8TGB90A>

---
+++ src/main/java/PWS_A/PWS_A/PwsAApplication.java
@@ -0,0 +1,13 @@
+package PWS_A.PWS_A;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PwsAApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PwsAApplication.class, args);
+	}
+
+}
---
+++ src/test/java/PWS_A/PWS_A/PwsAApplicationTests.java
@@ -0,0 +1,13 @@
+package PWS_A.PWS_A;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PwsAApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
